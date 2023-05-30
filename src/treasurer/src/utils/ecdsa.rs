e3e1b9fc531653157aa5a043e8c82a3565cc02c8
ivanlele <ivanlele402@gmail.com>
2023-05-30 13:32:31 +0000

    fix the rec_eth_addr

--- src/treasurer/src/utils/ecdsa.rs
+++ src/treasurer/src/utils/ecdsa.rs
@@ -1,12 +1,16 @@
 use super::*;
 
+use ic_cdk::api::call::call_with_payment;
+
+const ECDSA_SIGN_CYCLES: u64 = 23_000_000_000;
+
 pub async fn rec_eth_addr(msg: &str, sig: &str) -> Result<H160> {
     let siwe_canister = STATE.with(|state| state.borrow().siwe_signer_canister.clone());
 
     let msg = msg.to_string();
     let sig = sig.to_string();
 
-    let (signer,): (String,) = ic_cdk::call(siwe_canister, "get_signer", (msg, sig))
+    let (signer,): (String,) = call_with_payment(siwe_canister, "get_signer", (msg, sig), ECDSA_SIGN_CYCLES)
         .await
         .map_err(|(code, msg)| anyhow!("{:?}: {}", code, msg))?;
 
